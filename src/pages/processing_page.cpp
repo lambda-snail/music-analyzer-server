1199cc5ca17190d8c4d6838b646ff41f12faf848
lambda-snail <>
2025-07-05 12:03:34 +0200

    work on styling for file upload and view

--- src/pages/processing_page.cpp
+++ src/pages/processing_page.cpp
@@ -5,6 +5,7 @@
 #include <Wt/WContainerWidget.h>
 #include <Wt/WFileDropWidget.h>
 #include <Wt/WTemplate.h>
+#include <Wt/WText.h>
 
 LambdaSnail::todo::ProcessingPage::ProcessingPage()
 {
@@ -20,9 +21,17 @@ LambdaSnail::todo::ProcessingPage::ProcessingPage()
 
     // t->bindNew<todo_view>("todo", *m_current_item);
 
+
     m_FileView = t->bindNew<FileView>("file-list");
 
     m_FileDrop = t->bindNew<Wt::WFileDropWidget>("file-drop");
+    m_FileDrop->addNew<Wt::WText>("Drop Files Here");
+    m_FileDrop->setToolTip("Drop files here or click this component to upload from your hard drive");
+    m_FileDrop->removeStyleClass("Wt-filedropzone");
+    m_FileDrop->addStyleClass("ls-filedrop");
+    m_FileDrop->addStyleClass("d-flex");
+    m_FileDrop->addStyleClass("justify-content-center");
+    m_FileDrop->setAcceptDirectories(false);
 
     m_FileDrop->drop().connect([this](std::vector<Wt::WFileDropWidget::File*> const& files) {
         int const maxFiles   = 5;
@@ -32,6 +41,7 @@ LambdaSnail::todo::ProcessingPage::ProcessingPage()
                 m_FileDrop->cancelUpload(files[i]);
                 continue;
             }
+
 
             m_FileView->addFile(files[i]);
 
